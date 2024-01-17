aa048b0d4f41342e58ad445a3cdc93e3d1876130
Didi7777777 <142671905+Didi7777777@users.noreply.github.com>
2024-01-17 15:19:56 -0600

    Update fibonacci_tb.sv

--- fibonacci_tb.sv
+++ fibonacci_tb.sv
@@ -3,62 +3,56 @@
 module fibonacci_tb;
 
   logic clk; 
-  logic reset;
-  logic [15:0] din;
-  logic start;
+  logic reset = 1'b0;
+  logic [15:0] din = 16'h0;
+  logic start = 1'b0;
   logic [15:0] dout;
   logic done;
 
-  // Instantiate your design
+  // instantiate your design
   fibonacci fib(clk, reset, din, start, dout, done);
 
   // Clock Generator
-  always begin
-    clk = 1'b0;
-    #5;
-    clk = 1'b1;
-    #5;
+  always
+  begin
+	clk = 1'b0;
+	#5;
+	clk = 1'b1;
+	#5
   end
 
-  initial begin
-    // Reset
-    reset = 1'b1;
-    #10 reset = 1'b0;
-    
-    // Test Case 1: Input of 5
-    #20;
-    din = 16'd5;
-    start = 1'b1;
-    #10 start = 1'b0;
-    #10 wait (done == 1'b1);
-    $display("Input: %d, Fibonacci Number: %d", din, dout);
+  initial
+  begin
+	// Reset
+	#0 reset = 0;
+	#10 reset = 1;
+	#10 reset = 0;
+	
+	/* ------------- Input of 5 ------------- */
+	// Inputs into module/ Assert start
+	#10;
+	din = 16'd5;
+	start = 1'b1;
+	#10 start = 1'b0;
+	
+	// Wait until calculation is done	
+	#10 wait (done == 1'b1);
 
-    // Test Case 2: Input of 8
-    #30;
-    din = 16'd8;
-    start = 1'b1;
-    #10 start = 1'b0;
-    #10 wait (done == 1'b1);
-    $display("Input: %d, Fibonacci Number: %d", din, dout);
+	// Display Result
+	$display("-----------------------------------------");
+	$display("Input: %d", din);
+	if (dout === 5)
+	    $display("CORRECT RESULT: %d, GOOD JOB!", dout);
+	else
+	    $display("INCORRECT RESULT: %d, SHOULD BE: 5", dout);
 
-    // Test Case 3: Input of 9
-    #30;
-    din = 16'd9;
-    start = 1'b1;
-    #10 start = 1'b0;
-    #10 wait (done == 1'b1);
-    $display("Input: %d, Fibonacci Number: %d", din, dout);
 
-    // Test Case 4: Input of 13
-    #30;
-    din = 16'd13;
-    start = 1'b1;
-    #10 start = 1'b0;
-    #10 wait (done == 1'b1);
-    $display("Input: %d, Fibonacci Number: %d", din, dout);
+	/* ----------------------
+	   TEST MORE INPUTS HERE
+	   ---------------------
+	*/
 
-    // End of test
-    #10;
-    $stop;
+    // Done
+	$stop;
   end
 endmodule
