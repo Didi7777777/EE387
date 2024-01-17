6414007130c01d8cc7daa5592d86734a693a085f
Didi7777777 <142671905+Didi7777777@users.noreply.github.com>
2024-01-17 15:04:28 -0600

    Update fibonacci_tb.sv

--- fibonacci_tb.sv
+++ fibonacci_tb.sv
@@ -3,46 +3,46 @@
 module fibonacci_tb;
 
   logic clk; 
-  logic reset;
-  logic [15:0] din;
-  logic start;
+  logic reset = 1'b0;
+  logic [15:0] din = 16'h0;
+  logic start = 1'b0;
   logic [15:0] dout;
   logic done;
 
+  // Instantiate the Fibonacci module
   fibonacci fib(clk, reset, din, start, dout, done);
 
-  // Clock generator
+  // Clock Generator
   always begin
     clk = 1'b0;
-    #5 clk = 1'b1;
+    #5;
+    clk = 1'b1;
     #5;
   end
 
   initial begin
-    // Initialize
+    // Reset sequence
     reset = 1'b1;
-    start = 1'b0;
-    din = 16'h0;
     #10 reset = 1'b0;
 
-    // Set input to 5 and start the module
-    #10;
-    din = 5;
-    start = 1'b1;
-    #10 start = 1'b0;
+    // Testing with input of 5
+    #20; // Wait for a stable state
+    din = 16'd5; // Set input to 5
+    start = 1'b1; // Start the calculation
+    #10 start = 1'b0; // Stop the start signal
 
     // Wait for calculation to complete
-    wait(done);
+    wait (done == 1'b1);
 
-    // Display the result
+    // Display results
     $display("-----------------------------------------");
     $display("Input: %d", din);
-    if (dout === 5) // Checking if the output is correct
+    if (dout === 5)
         $display("CORRECT RESULT: %d, GOOD JOB!", dout);
     else
         $display("INCORRECT RESULT: %d, SHOULD BE: 5", dout);
 
-    // End simulation
-    #100 $stop;
+    // End of test
+    $stop;
   end
 endmodule
