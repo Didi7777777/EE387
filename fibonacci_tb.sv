1365216f312368e974cc135fcbbad9d7b5dd42c2
Didi7777777 <142671905+Didi7777777@users.noreply.github.com>
2024-01-17 23:52:41 -0600

    Update fibonacci_tb.sv

--- fibonacci_tb.sv
+++ fibonacci_tb.sv
@@ -13,82 +13,77 @@ module fibonacci_tb;
   fibonacci fib(clk, reset, din, start, dout, done);
 
   // Clock Generator
-  always
-  begin
-	clk = 1'b0;
-	#5;
-	clk = 1'b1;
-	#5
+  always begin
+    clk = 1'b0;
+    #5;
+    clk = 1'b1;
+    #5;
   end
 
-  initial
-  begin
-	// Reset
-	#0 reset = 0;
-	#10 reset = 1;
-	#10 reset = 0;
-	
-	/* ------------- Input of 5 ------------- */
-	// Inputs into module/ Assert start
-	#10;
-	din = 16'd5;
-	start = 1'b1;
-	#10 start = 1'b0;
-	
-	// Wait until calculation is done	
-	#10 wait (done == 1'b1);
+  initial begin
+    // Common reset sequence for each test
+    task common_reset;
+      begin
+        reset = 1'b1; // Assert reset
+        #20;          // Wait for reset to take effect
+        reset = 1'b0; // Deassert reset
+        #20;          // Wait for deassertion to stabilize
+      end
+    endtask
 
-	// Display Result
-	$display("-----------------------------------------");
-	$display("Input: %d", din);
-	if (dout === 5)
-	    $display("CORRECT RESULT: %d, GOOD JOB!", dout);
-	else
-	    $display("INCORRECT RESULT: %d, SHOULD BE: 5", dout);
+    // Test sequence for a given input
+    task perform_test(input [15:0] test_input);
+      begin
+        din = test_input;      // Set test input
+        start = 1'b1;          // Start calculation
+        #10;                   // Wait for start signal to be acknowledged
+        start = 1'b0;          // Stop start signal
+        wait(done == 1'b1);    // Wait for calculation to complete
+        #10;                   // Additional wait time, if necessary
+      end
+    endtask
 
+    // Reset
+    common_reset();
 
-	/* ----------------------
-	   TEST MORE INPUTS HERE
-	   ---------------------
-	*/
+    /* ------------- Input of 5 ------------- */
+    perform_test(16'd5);
 
-	/* ------------- Input of 8 ------------- */
-	// Inputs into module/ Assert start
-	#10;
-	din = 16'd8;
-	start = 1'b1;
-	#10 start = 1'b0;
-	
-	// Wait until calculation is done	
-	#10 wait (done == 1'b1);
+    // Display Result
+    $display("-----------------------------------------");
+    $display("Input: %d", din);
+    if (dout === 5)
+      $display("CORRECT RESULT: %d, GOOD JOB!", dout);
+    else
+      $display("INCORRECT RESULT: %d, SHOULD BE: 5", dout);
 
-	// Display Result
-	$display("-----------------------------------------");
-	$display("Input: %d", din);
-	if (dout === 21)
-	    $display("CORRECT RESULT: %d, GOOD JOB!", dout);
-	else
-	    $display("INCORRECT RESULT: %d, SHOULD BE: 21", dout);
+    common_reset();
 
+    /* ------------- Input of 8 ------------- */
+    perform_test(16'd8);
 
-	/* ------------- Input of 9 ------------- */
-	// Inputs into module/ Assert start
-	#10;
-	din = 16'd9;
-	start = 1'b1;
-	#10 start = 1'b0;
-	
-	// Wait until calculation is done	
-	#10 wait (done == 1'b1);
+    // Display Result
+    $display("-----------------------------------------");
+    $display("Input: %d", din);
+    if (dout === 21)
+      $display("CORRECT RESULT: %d, GOOD JOB!", dout);
+    else
+      $display("INCORRECT RESULT: %d, SHOULD BE: 21", dout);
 
-	// Display Result
-	$display("-----------------------------------------");
-	$display("Input: %d", din);
-	if (dout === 34)
-	    $display("CORRECT RESULT: %d, GOOD JOB!", dout);
-	else
-	    $display("INCORRECT RESULT: %d, SHOULD BE: 34", dout);
+    common_reset();
+
+    /* ------------- Input of 9 ------------- */
+    perform_test(16'd9);
+
+    // Display Result
+    $display("-----------------------------------------");
+    $display("Input: %d", din);
+    if (dout === 34)
+      $display("CORRECT RESULT: %d, GOOD JOB!", dout);
+    else
+      $display("INCORRECT RESULT: %d, SHOULD BE: 34", dout);
+
     // Done
-	$stop;
+    $stop;
   end
 endmodule
